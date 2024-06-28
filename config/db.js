d6759ca7b87616e43d7bb3434265c0d84717fdf4
Pradeep Kumar <pradeepmahto7991@gmail.com>
2024-06-28 17:46:04 +0530

    mongoURL fixed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URL);
+    const con = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`connected to databases${con.connection.host}`);
   } catch (error) {
     console.log(error);
